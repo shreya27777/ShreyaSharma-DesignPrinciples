e9c34842bd9a3f8cf7c532e5ad3642d5ebca1d06
shreya sharma <shreyasharmacu@gmail.com>
2020-02-11 13:30:44 +0530

    COMMIT

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        double num1, num2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number:");
+        num1 = scanner.nextDouble();
+        System.out.print("Enter second number:");
+        num2 = scanner.nextDouble();
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char op = scanner.next().charAt(0);
+        double output;
+        switch(op)
+        {
+            case '+':
+                output = num1 + num2;
+                break;
+            case '-':
+                output = num1 - num2;
+                break;
+            case '*':
+                output = num1 * num2;
+                break;
+            case '/':
+                output = num1 / num2;
+                break;
+            default:
+                System.out.printf("You have entered wrong operator");
+                return;
+        }
+        System.out.println(num1+" "+op+" "+num2+": "+output);
+    }
+}
